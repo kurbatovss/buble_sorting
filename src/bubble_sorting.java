17da12b4ee74eca3fb9ce111e2b96ed050b358df
Sergofans <kssdevspace@gmail.com>
2025-05-16 21:29:52 +0600

    bubble sorting project created

---
+++ src/bubble_sorting.java
@@ -0,0 +1,35 @@
+public class bubble_sorting {
+    public static void main(String[] args) {
+        int min = -100; // нижняя граница массива
+        int max = 100; // верхняя граница массива
+        int nums[] = new int[12];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (int) (Math.random() * (max - min + 1)) + min;
+        }
+        display(nums);
+        System.out.println("\n\nПроизведена сортировка");
+        sortArray(nums);
+    }
+
+    private static void display(int[] arik) {
+        System.out.println("Инициализирован массив:");
+        for (int i = 0; i < arik.length; i++) {
+            System.out.print(arik[i] + " ");
+        }
+    }
+    private static void sortArray(int[] arik) {
+        System.out.println("\nОтсортированный массив:");
+        for (int i = 1; i < arik.length; i++) {
+            for ( int j = 0; j < arik.length-1; j++) {
+                if ( arik[j] > arik[j+1] ) {
+                    int k = arik[j+1];
+                    arik[j+1] = arik[j];
+                    arik[j] = k;
+                }
+            }
+        }
+        for (int i = 0; i < arik.length; i++) {
+            System.out.print(arik[i] + " ");
+        }
+    }
+}
